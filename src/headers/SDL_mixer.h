53279dc600c60c9b62530178fd2631ddff9b405a
alxmrz <alexandr.moroz97@mail.ru>
2024-05-24 20:18:22 +0300

    removed comments

--- src/headers/SDL_mixer.h
+++ src/headers/SDL_mixer.h
@@ -11,11 +11,9 @@ typedef struct Mix_Chunk {
 typedef struct _Mix_Music Mix_Music;
 
 int Mix_OpenAudio(int frequency, Uint16 format, int channels, int chunksize);
-//Mix_Chunk * Mix_LoadWAV(const char *file);
+
 Mix_Chunk * Mix_LoadWAV_RW(void *src, int freesrc);
 Mix_Music * Mix_LoadMUS(const char *file);
 int Mix_PlayMusic(Mix_Music *music, int loops);
-//#define Mix_PlayChannel(channel,chunk,loops) Mix_PlayChannelTimed(channel,chunk,loops,-1)
-  /* The same as above, but the sound is played at most 'ticks' milliseconds */
+
 int Mix_PlayChannelTimed(int channel, Mix_Chunk *chunk, int loops, int ticks);
-//int Mix_PlayChannel(int channel, Mix_Chunk *chunk, int loops);
